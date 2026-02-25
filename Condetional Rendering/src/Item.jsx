0b1f96815815ba88099b1d89cbae3bf8239098b0
ShivamBariCollege <mca-reg202527004@timscdrmumbai.in>
2026-02-25 08:32:25 +0000

    used ternery operator

--- Condetional Rendering/src/Item.jsx
+++ Condetional Rendering/src/Item.jsx
@@ -8,15 +8,18 @@
 // }
 
 
-function Item({name="Bags",isPacked=true}){
-    if(isPacked){
-        return <li>{name} + 👍</li>
-    }
-    else{
-        return  <li>{name} </li>
-    }
+function Item({name="Bags",isPacked=false}){
+// ternery operator 
+    return (
+        <li>{name} {isPacked? "👍":  "❤️"}</li>
+    )
+    // if(isPacked){
+    //     return <li>{name} + 👍</li>
+    // }
+    // else{
+    //     return  <li>{name} </li>
+    // }
 }
 export default Item
 
-// ternery operstor 
-//  <li>{name} {isPacked? "👍":  "❤️"}</li>
+
